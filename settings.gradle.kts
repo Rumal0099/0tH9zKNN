8780dab65c9ddcca633a43398c048bb26c218c66
Rumal Hewapathirana <99350356+Rumal0099@users.noreply.github.com>
2023-09-13 21:41:17 +0530

    Added search function to the interface and done some changes in design

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Personal Notes Manager_IT20642396"
+include(":app")
+ 
