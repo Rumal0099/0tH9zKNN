8d71494de4a271654efb635faf9fc57beb3d0d53
Rumal Hewapathirana <99350356+Rumal0099@users.noreply.github.com>
2023-09-17 13:21:59 +0530

    Added search function to the interface and done some changes in design

---
+++ app/src/main/java/com/example/personalnotesmanager_it20642396/Add_Edit_Note_Screen.kt
@@ -0,0 +1,11 @@
+package com.example.personalnotesmanager_it20642396
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_Edit_Note_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_note_screen)
+    }
+}
